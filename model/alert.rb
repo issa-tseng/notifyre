e05a9ba4fd7d7ce1db022d102dc3dcfefa2d57c5
Chris Metcalf <chris@chrismetcalf.net>
2010-08-22 15:17:29 -0700

    geocoding

--- model/alert.rb
+++ model/alert.rb
@@ -1,8 +1,19 @@
+require 'geokit'
+
 class Alert < Sequel::Model
   many_to_one :user
 
   def self.trigger(fyre)
     Alert.filter("range(cast(latitude as numeric), cast(longitude as numeric), cast(#{fyre.latitude} as numeric), cast(#{fyre.longitude} as numeric)) <= radius")
+  end
+
+  def before_save
+    addr = Geokit::Geocoders::YahooGeocoder.geocode self.human_address
+
+    if !addr.nil?
+      self.latitude = addr.lat
+      self.longitude = addr.lng
+    end
   end
 end
 
