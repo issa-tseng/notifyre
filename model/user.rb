c8b62a1aacdb345c7bedc9e21277ca9224b081c4
Chris Metcalf <chris@chrismetcalf.net>
2010-08-22 16:10:01 -0700

    slightly broken user signup model

--- model/user.rb
+++ model/user.rb
@@ -12,7 +12,7 @@ class User < Sequel::Model
   one_to_many :alerts
 
   def before_create
-    self.password (String.random_char 10)
+    self.set_password(String.random_chars(10))
     Tropo.send_text(self.phone_number,'Reply with \"VERIFY\" to complete signup.  Your password is: '+self.password)
     self.salt = Time.now.to_f.to_s
   end
@@ -27,12 +27,12 @@ class User < Sequel::Model
 
   def reset_password!
     new_password = (String.random_chars 10)
-    self.password = new_password
+    self.set_password(new_password)
     return new_password
   end
 
-  def password=(plaintext)
-    self.password = (User.hash_password plaintext, self.salt)
+  def set_password(plaintext)
+    self.password = User.hash_password(plaintext, self.salt)
   end
 
   def authenticate?(plaintext)
