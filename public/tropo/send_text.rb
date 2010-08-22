c0b586ce8aba5e5bb5470e422219ff4200d91aee
Temporary user for Socrata Hackathon <hack@airplane.(none)>
2010-08-22 21:34:10 +0000

    minor changes to make it a little less wrong.

--- public/tropo/send_text.rb
+++ public/tropo/send_text.rb
@@ -1,14 +1,16 @@
 #
-# This file is hosted on Tropo's servers
+# this file is hosted on Tropo's servers
 #
 if $action == "create"
     if defined? $msg || defined? $to || $msg == "" || $to == ""
         log("Something blank")
     else
         message($msg, {
-            :to => $to,
+            :to => "tel:+"+$to,
             :network => "SMS"})
     end
 else
+    message("This is notifyre.com. Visit for our site for more info.", {:to => $currentCall.callerID, :network => "SMS"})
     log("Message from " + $currentCall.callerID + ": " + $currentCall.initialText)
 end
+
